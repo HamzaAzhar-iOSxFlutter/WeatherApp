109c2f14ba03a3ace06bb4353643cf619a9b794e
hamzaazharxiOS <hamzaazharcs@gmail.com>
2023-08-01 21:35:11 -0400

    Location Manager incorporated

--- WeatherApp/WeatherApp/WeatherViewController.swift
+++ WeatherApp/WeatherApp/WeatherViewController.swift
@@ -6,9 +6,20 @@
 //
 
 import UIKit
+import MapKit
 
 class WeatherViewController: UIViewController {
+    
+    var locManager = CLLocationManager()
+    var currentLocation: CLLocation!
 
+    @IBOutlet weak var labelTemperature: UILabel!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var buttonSearch: UIButton!
+    @IBOutlet weak var buttonLocation: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
